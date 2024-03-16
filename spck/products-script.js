7009c485683c3d95a9569aa6956a260b1d9b959b
Centrelmoy <103297734+Centrelmoy@users.noreply.github.com>
2024-03-16 15:29:42 +0700

    fix

--- spck/products-script.js
+++ spck/products-script.js
@@ -56,37 +56,37 @@
 // getData();
 
 
-const productContainer = document.querySelector(".product-list");
-const url = "https://shoes-collections.p.rapidapi.com/shoes";
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "3dfa1a0dc4mshc4c705f8f82848dp1c28ffjsn76b6e1ca1d8c",
-    "X-RapidAPI-Host": "shoes-collections.p.rapidapi.com",
-  },
-};
-async function getData() {
-  try {
-    const response = await fetch(url, options);
-    const result = await response.text();
+// const productContainer = document.querySelector(".product-list");
+// const url = "https://shoes-collections.p.rapidapi.com/shoes";
+// const options = {
+//   method: "GET",
+//   headers: {
+//     "X-RapidAPI-Key": "3dfa1a0dc4mshc4c705f8f82848dp1c28ffjsn76b6e1ca1d8c",
+//     "X-RapidAPI-Host": "shoes-collections.p.rapidapi.com",
+//   },
+// };
+// async function getData() {
+//   try {
+//     const response = await fetch(url, options);
+//     const result = await response.text();
 
-    //  Em bị quên đoạn này nè
-    data = JSON.parse(result);
-    // 
+//     //  Em bị quên đoạn này nè
+//     data = JSON.parse(result);
+//     // 
     
-    data.forEach((product) => {
-      const productCard = document.createElement("div");
-      productCard.classList.add("product-card");
-      productCard.innerHTML = `
-        <img src="${product.image}" alt="${product.name} style="width: 100px; height: 200px;">
-        <h3>${product.name}</h3>
-        <p>${product.price}</p>
-        <button>Add to Cart</button>
-      `;
-      productContainer.appendChild(productCard);
-    });
-  } catch (error) {
-    console.error(error);
-  }
-}
-getData();
+//     data.forEach((product) => {
+//       const productCard = document.createElement("div");
+//       productCard.classList.add("product-card");
+//       productCard.innerHTML = `
+//         <img src="${product.image}" alt="${product.name} style="width: 100px; height: 200px;">
+//         <h3>${product.name}</h3>
+//         <p>${product.price}</p>
+//         <button>Add to Cart</button>
+//       `;
+//       productContainer.appendChild(productCard);
+//     });
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+// getData();
